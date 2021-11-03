a83e967e7240823e46bd2afc8e52f803c362dcab
vivek <vivek@vivekBhand.org>
2021-11-03 18:55:32 +0530

    Commit other extra code on Vivek's side

--- patient.py
+++ patient.py
@@ -1,40 +1,99 @@
-from os.path import exists
-import os
 
-details = ['Name', 'Age', 'Gender','Phone', 'Bloodgroup', 'Sugar','BloodPressure','Vaccine','VaccineName']
-validLength = { "Phone":10,"Vaccine":1}
 
-error = {}
-def menu():
-    filename = 'patient.txt'
-    if exists(filename):
-        fp = open(filename,'r')
-        # print("fg")
-        # print(fp)
-        # print(filename)
-    else:  
-        print("File doesn't exist")
-        error.update({'NoFile':"NotFound"})
-        return
+def name(pname):
+	fp.write(pname + ",")
+	return
 
-    lines = fp.readlines()
-    # print(lines)
-    count = 0
-    for line in lines:
-        dataStudent = line.split(',')
-        for i,l in enumerate(details):
-            if l == 'Phone' or l == 'Vaccine':
-                if len(dataStudent[i]) != validLength[l]:
-                    entry = ("At entry "+str(count))
-                    error.update({entry:'Valid Length Error ' + details[i] + " "})
-            if l == 'Vaccine' and dataStudent[i] == '0':
-                if dataStudent[i+1] != "NA":
-                    entry = ("At entry " + str(count))
-                    error.update({entry:"Vaccine Information Error"})
-        count +=1
+def age(page):
+	fp.write(page + ",")
+	return
 
-    for i in error:
-        print(i," : ",error[i])
+def gender(pgender):
+	fp.write(pgender + ",")
+	return
+	
+def phone(pphone):
+	fp.write(pphone + ",")
+	return
+	
+def bloodgroup(pbloodgroup):
+	fp.write(pbloodgroup + ",")
+	return
 
-if __name__== "__main__":
-    menu()
+def sugar(psugar):
+	fp.write(psugar + ",")
+	return
+
+def bloodpressure(pbloodpressure):
+	fp.write(pbloodpressure + ",")
+	return
+
+def vaccine(pvaccine):
+	fp.write(pvaccine + ",")
+	return
+
+def vaccinename(pvaccinename):
+	fp.write(pvaccinename)
+	fp.write("\n")
+	return
+
+	
+	
+while True:
+	print("\n")
+	print("1.Add new information")
+	print("2.Display infomation from the file")
+	print("3.Exit")
+	print("\n")
+	choice = int(input("Enter our choice : "))
+	
+	if(choice == 1):
+		filename = input("Enter the name of the file: ")
+		fp = open(filename,"a")
+		
+		pname = input("Enter patient name: ") 
+		name(pname)
+		
+		page = input("Enter age: ")
+		age(page)
+
+		pgender = input("Enter gender(M/F): ")
+		gender(pgender)
+
+		pphone = input("Enter phone no: ")
+		phone(pphone)
+
+		pbloodgroup = input("Enter blood group(A/B/AB/O): ")
+		bloodgroup(pbloodgroup)
+
+		psugar = input("Enter sugar(Y/N): ")
+		sugar(psugar)
+
+		pbloodpressure = input("Enter bloodpressure: ")
+		bloodpressure(pbloodpressure)
+
+		pvaccine = input("Enter vaccine(0/1/2): ")
+		vaccine(pvaccine)
+
+		pvaccinename = input("Enter vaccine name: ")
+		vaccinename(pvaccinename)
+		
+		fp.close()
+		
+	elif(choice == 2):
+		filename2 = input("Enter the name of the file to read information: ")
+		print("\n")
+		try:
+			fp2 = open(filename2,"r")
+			print(fp2.read())
+			print("\n")
+			fp2.close()
+		except FileNotFoundError:
+			print("Oops there is no file named : ",filename2)
+					
+	elif(choice == 3):
+		exit(1)
+	else:
+		print("\nOops incorrect choice!")
+		print("Try again\n")
+		
--- staff.py
+++ staff.py
@@ -2,6 +2,7 @@
 #and also display the information from the file
 import datetime
 from datetime import date
+from datetime import datetime
 
 def id(sid):
 	fp.write(sid + ",")
@@ -29,20 +30,35 @@ def designation(sdesignation):
 	return
 
 def phone(sphone):
-    fp.write(sphone)
-    fp.write("\n")
+    fp.write(sphone + ",")
+    # fp.write("\n")
     return
+
+def salary(ssalary):
+	fp.write(ssalary + ",")
+	return
+
+def empty(sempty):
+	fp.write(" ")
+	fp.write("\n")
+	return
  
-def validate(date_string):
+'''def validate(date_string):
 	#date_string = '12-25-2018'
 	format = "%Y-%m-d"
 	try:
 		datetime.datetime.strptime(date_string, format)
 		print("This is the correct date string format.")
 	except ValueError:
-		print("This is the incorrect date string format. It should be YYYY-MM-DD")
-        
-
+		print("This is the incorrect date string format. It should be YYYY-MM-DD")'''
+		     
+def validate(date_string):
+	format = "%Y-%m-%d"
+	try:
+		res = bool(datetime.strptime(date_string, format))
+	except ValueError:
+		res = False
+	return res
 	
 	
 while True:
@@ -55,8 +71,8 @@ while True:
 	choice = int(input("Enter our choice : "))
 	
 	if(choice == 1):
-		filename = input("Enter the name of the file: ")
-		fp = open(filename,"a")
+		# filename = input("Enter the name of the file: ")
+		fp = open("staff.csv","a")
 		
 		sid = input("Enter staff id: ")
 		id(sid)
@@ -78,60 +94,167 @@ while True:
 
 		sphone = input("Enter phone no: ")
 		phone(sphone)
+
+		ssalary = input("Enter the salary: ")
+		salary(ssalary)
 		
+		sempty = input("Press something")
+		empty(sempty)
+
 		fp.close()
 		
 	elif(choice == 2):
-		filename2 = input("Enter the name of the file to read information: ")
+		# filename2 = input("Enter the name of the file to read information: ")
 		print("\n")
 		try:
-			fp2 = open(filename2,"r")
+			fp2 = open("staff.csv","r")
 			print(fp2.read())
 			print("\n")
 			fp2.close()
 		except FileNotFoundError:
-			print("Oops there is no file named : ",filename2)
+			print("Oops there is no file named : ","staff.csv")
 					
 	elif(choice == 3):
-		filename3 = input("Enter the name of the file where you stored the INFO: ")
+		# filename3 = input("Enter the name of the file where you stored the INFO: ")
 		print("\n")
 		print("-----------------")
 		print("TEST CASE CHECK RESULT")
-		fp3 = open(filename3,"r")
-		line=fp3.read()
+		fp3 = open("staff.csv","r")
+		lines=fp3.readlines()
+		print(lines)
+		# a1 = 1
 		#print(line)
-		line =line.split(",")
-		#print(len(line))
-		if (line[0]).isnumeric() :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE ID ")
-		if type(line[1])==str:
-			print("OK")
-		else:
-			print("PLEASE CHECK THE NAME ")
-		if (line[2]).isnumeric() :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE AGE ")
-		if line[3]=="M" or line[3]=="F" :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE GENDER ")
-		if validate(line[4]) :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE DATE ")
-		if line[5]=="D" or line[5]=="N"  :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE DESIGNATION ")
-		if (len(line[6])-1)==10 :
-			print("OK")
-		else:
-			print("PLEASE CHECK THE NUMBER ")
-		print("----------------")
-		#print(line[7])
+		for line in lines:
+			print(line)
+			line = line.split(",")
+			print(line)
+			# print(a1)
+			#print(len(line))
+			if (line[0]).isnumeric() :
+				print("\033[0;32m")
+				print("Passed : OK")
+				a1 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE ID ")
+				a1 = 0
+			if type(line[1])==str:
+				print("\033[0;32m")
+				print("Passed : OK")
+				a2 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE NAME ")
+				a2 = 1
+			"""
+			for i in line[2]:
+				if (ord(i)>47) and (ord(i)<58):
+					ok = 0
+				else:
+					ok = 1
+			if ok == 0:
+				print("\033[0;32m")
+				print("Passed : OK")
+				a3 = -1
+			"""
+			if (line[2]).isnumeric():
+				print("\033[0;32m")
+				print("Passed : OK")
+				a3 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE AGE ")
+				a3 = 2
+			if line[3]=="M" or line[3]=="F" :
+				print("\033[0;32m")
+				print("Passed : OK")
+				a4 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE GENDER ")
+				a4 = 3
+			# print(line[4])
+			if validate(line[4]) :
+				print("\033[0;32m")
+				print("Passed : OK")
+				a5 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE DATE ")
+				a5 = 4
+			if line[5]=="D" or line[5]=="N":
+				print("\033[0;32m")
+				print("Passed : OK")
+				a6 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE DESIGNATION ")
+				a6 = 5
+			if (len(line[6]))==10 :
+				print("\033[0;32m")
+				print("Passed : OK")
+				a7 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE NUMBER ")
+				a7 = 6
+			if line[7].isnumeric() :
+				print("\033[0;32m")
+				print("Passed : OK")
+				a8 = -1
+			else:
+				print("\033[0;31m")
+				print("Failed : PLEASE CHECK THE SALARY ")
+				a8 = 7
+			print("----------------")
+
+			print("\033[0;37m")
+			#updating code
+			if a1 == -1:
+				pass
+			else:
+				id = input("Enter the ID again : ")
+				line[a1] = id
+			if a2 == -1:
+				pass
+			else:
+				name = input("Enter the name again : ")
+				line[a2] = name
+			if a3 == -1:
+				pass
+			else:
+				age = input("Enter the age again : ")
+				line[a3] = age
+				print(line[a3])
+				# break
+			if a4 == -1:
+				pass
+			else:
+				gender = input("Enter the gender again : ")
+				line[a4] = gender
+			if a5 == -1:
+				pass
+			else:
+				date = input("Enter the joining date again : ")
+				line[a5] = date
+			if a6 == -1:
+				pass
+			else:
+				designation = input("Enter the designation again : ")
+				line[a6] = designation
+			if a7 == -1:
+				pass
+			else:
+				phone = input("Enter the phone no. again : ")
+				line[a7] = phone
+			if a8 == -1:
+				pass
+			else:
+				salary = input("Enter the salary again : ")
+				line[a8] = salary
+			#print(line[7])
+			print("\033[0;37m")
+			# a1 += 1
 		fp3.close()
 		
 		
